a0c784818e458d428745d4f0d4bb4bcf051cc0f4
Kenan Kadic <ken.kadic@outlook.com>
2022-06-14 08:11:40 -0500

    update to challange 2

--- challenge.js
+++ challenge.js
@@ -1,4 +1,4 @@
-// // RESTAURANT BILL
+// Challenge 1 RESTAURANT BILL
 //
 // let billOne = 275;
 // let tip = billOne <= 300 && billOne >= 50 ? billOne * .15 : billOne *.2
@@ -9,19 +9,35 @@
 // let billTwo = 40;
 // let BillThree = 430;
 
-function calcAverage(scoreOne, scoreTwo, scoreThree, teamName) {
-    let averageValue = (scoreOne + scoreTwo + scoreThree) / 3;
-    return ` The average for the ${teamName} is ${averageValue}`
-}
+// Challenge 2 - Averages
+//
+// function calcAverage(scoreOne, scoreTwo, scoreThree, teamName) {
+//     let averageValue = (scoreOne + scoreTwo + scoreThree) / 3;
+//     return `The average for the ${teamName} is ${averageValue}`
+// }
+//
+// let averageDolphins = calcAverage(44,23,71, 'Dolphins')
+// console.log(averageDolphins)
+//
+// let averageKoalas = calcAverage(85,54,41, 'Koalas')
+// console.log(averageKoalas)
+//
+// function checkWinner(avgDolphins, avgKoalas) {
+//     if (avgDolphins > avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
+//     } else if (avgKoalas > avgDolphins) {
+//         console.log(`Koalas win (${avgKoalas} vs ${avgDolphins} )`)
+//     } else {
+//         console.log('the score was tied')
+//     }
+// }
+//
+// let averageDolphins2 = calcAverage(85,54,41, 'Dolphins2')
+// console.log(averageDolphins2)
+//
+// let averageKoalas2 = calcAverage(23,34,27, 'Koalas2')
+// console.log(averageKoalas2)
+//
+// checkWinner(averageDolphins, averageKoalas)
+// checkWinner(averageKoalas2, averageDolphins2)
 
-let averageDolphins = calcAverage(44,23,71, 'Dolphins')
-console.log(averageDolphins)
-
-let averageKoalas = calcAverage(85,54,41, 'Koalas')
-console.log(averageKoalas)
-
-let averageDolphins2 = calcAverage(85,54,41, 'Dolphins2')
-console.log(averageDolphins2)
-
-let averageKoalas2 = calcAverage(23,34,27, 'Koalas2')
-console.log(averageKoalas2)
--- main.js
+++ main.js
@@ -178,18 +178,18 @@ const calcAge = function(birthYear) {
     return 2037 - birthYear;
 }
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-    const age = calcAge(birthYear)
-    const retirement = 65 - age;
-    // return retirement;
-        if (retirement > 0) {
-            console.log(`${firstName} retires in ${retirement} years`);
-            return retirement;
-        } else {
-            console.log(`${firstName} has already retired`);
-            return -1;
-        }
-}
-
-console.log (yearsUntilRetirement(1991, 'Jonas'));
-console.log (yearsUntilRetirement(1950, 'Mike'));
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear)
+//     const retirement = 65 - age;
+//     // return retirement;
+//         if (retirement > 0) {
+//             console.log(`${firstName} retires in ${retirement} years`);
+//             return retirement;
+//         } else {
+//             console.log(`${firstName} has already retired`);
+//             return -1;
+//         }
+// }
+//
+// console.log (yearsUntilRetirement(1991, 'Jonas'));
+// console.log (yearsUntilRetirement(1950, 'Mike'));
